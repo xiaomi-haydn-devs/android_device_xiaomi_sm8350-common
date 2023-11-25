9aa6fe456ff27efe6f924dc4d624203eeabda55f
Astridxx <muratkozan350@gmail.com>
2023-11-25 23:15:53 +0300

    sm8350-common: Add RisingOS dependencies

--- common.mk
+++ common.mk
@@ -404,7 +404,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-rising
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
