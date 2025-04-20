a87521412b61fd88f9c2b63d61daaf6d23d8bf01
Alucard-Storm <supersayan1000@gmail.com>
2025-04-20 17:36:12 +0530

    sm8350-common: drop non-existing omx packages
    FAILED:
    build/make/core/main.mk:1117: warning:  device/xiaomi/haydn/lineage_haydn.mk includes non-existent modules in PRODUCT_PACKAGES
    Offending entries:
    libOmxAacEnc
    libOmxAmrEnc
    libOmxCore
    libOmxEvrcEnc
    libOmxG711Enc
    libOmxQcelp13Enc
    libbthost_if
    libbthost_if.vendor
    libldacBT_bco.vendor
    libmm-omxcore
    libstagefrighthw
    build/make/core/main.mk:1117: error: Build failed.

--- common.mk
+++ common.mk
@@ -99,9 +99,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
     com.dsi.ant@1.0.vendor \
-    libbthost_if \
-    libbthost_if.vendor \
-    libldacBT_bco.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
@@ -330,14 +327,6 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_PACKAGES += \
     libcodec2_hidl@1.0.vendor \
-    libmm-omxcore \
-    libOmxAacEnc \
-    libOmxAmrEnc \
-    libOmxCore \
-    libOmxEvrcEnc \
-    libOmxG711Enc \
-    libOmxQcelp13Enc \
-    libstagefrighthw \
     android.hardware.media.c2@1.1.vendor \
     android.hardware.media.c2@1.2.vendor \
     libavservices_minijail.vendor \
